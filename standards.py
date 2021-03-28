150160dedf990cc31d4d28a680503f5b84cf1584
M. Fatih MAR <mfatihmar@gmail.com>
2021-03-28 03:51:36 +0100

    remove placeholder descriptions from standards script

--- standards.py
+++ standards.py
@@ -7,9 +7,9 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-i", "--install", help="install help text here (todo)", action="store_true")
-parser.add_argument("-c", "--check", help="check help text here (todo)", action="store_true")
-parser.add_argument("-f", "--fix", help="fix help text here (todo)", action="store_true")
+parser.add_argument("-i", "--install", action="store_true")
+parser.add_argument("-c", "--check", action="store_true")
+parser.add_argument("-f", "--fix", action="store_true")
 
 if len(sys.argv) == 1:
     parser.print_help(sys.stderr)
